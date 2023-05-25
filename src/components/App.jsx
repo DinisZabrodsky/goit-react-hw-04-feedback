0d21165d3269a4cb47ef8989649520abb0c2aa9d
DinisZabrodsky <115039213+DinisZabrodsky@users.noreply.github.com>
2023-05-25 19:40:26 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+import { Component } from "react";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import Notification  from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+  }
+
+  addFeedback = ({target: {dataset: {name}}}) => {
+    this.setState((pre) => ({
+        [name]: pre[name] + 1,
+    }))
+
+    this.countTotalFeedback()
+  }
+
+  countTotalFeedback = () => {
+    this.setState((pre) => ({
+        total: pre.total + 1,
+    }))
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    return (this.state.good)/(this.state.total) * 100
+  }
+
+
+render () {
+    return (<div>
+            <h1>Залишіть відгук</h1>
+            <div>
+              <FeedbackOptions options={["good", "neutral", "bad"]} onLeaveFeedback={this.addFeedback}/>
+            </div>
+
+            {
+                this.state.total === 0 ? 
+                <Notification  message="There is no feedback"/>: 
+                <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.state.total} positivePercentage={this.countPositiveFeedbackPercentage}/>
+            }
+            </div>)}
 };
